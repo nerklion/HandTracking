0f1b757cf5ad355c41edb0ec57b9e8ffb52101c4
Matthew <74854708+nerklion@users.noreply.github.com>
2021-08-21 20:23:23 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from AIVirtualMouse import virt_mouse_main
+
+print("Info: enter app num to run it"
+      "©nerklion, 2021")
+
+app = input("Apps: "
+            "[1] - Virtual Mouse ")
+
+if int(app) == 1:
+    virt_mouse_main()
+else:
+    pass
